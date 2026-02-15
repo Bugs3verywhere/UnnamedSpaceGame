d8cdc9c65f11dcc4797cd559ab2c9c60cc4d9b73
Oliver Wood <oliwood2468@gmail.com>
2026-02-15 20:32:18 +0000

    Intro Sequence
    Basic intro sequence, player and drop pod float down from the sky, function added in script to allow for player movement once intro cutscene completes

---
+++ ProjectFile/Assets/IntroSequence.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class IntroSequence : MonoBehaviour
+{
+
+    public GameObject dropPod;
+    public GameObject playerController;
+    public Rigidbody playerRigidbody;
+
+    void Start()
+    {
+        playerController.transform.DOMoveY(1.125f, 20, false)
+        .SetEase(Ease.Linear);
+        dropPod.transform.DOMoveY(0, 20, false)
+        .SetEase(Ease.Linear)
+        .OnComplete(CutsceneEnd);
+    }
+
+    public void CutsceneEnd()
+    {
+        //Allow player movement, unlock camera etc.
+    }
+}
